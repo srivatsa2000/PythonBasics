fa48c8eb98a7d7918bd4b98847a0f42f9d610efe
Srivatsa N <11293760+srivatsa2000@users.noreply.github.com>
2020-10-18 19:54:52 +0530

    Negating the boolean values

--- IfUses.py
+++ IfUses.py
@@ -1,5 +1,6 @@
 housePrice = 1000
-buyerHasGoodCredit = True
+buyerHasGoodCredit = False
+highIncome = False
 downPayment = 0
 
 if buyerHasGoodCredit:
@@ -7,3 +8,14 @@ if buyerHasGoodCredit:
 else:
     downPayment = housePrice * 0.2
 print(f'Down payment value is {downPayment}')
+
+if buyerHasGoodCredit and highIncome:
+    print('Both Should be True')
+
+if buyerHasGoodCredit or highIncome:
+    print('Or condition')
+
+# Negating the values
+
+if not buyerHasGoodCredit:
+    print('No loan')
