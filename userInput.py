bf01ffd7e3559569d8263b9f97ad16b1e9fb4508
Srivatsa N <11293760+srivatsa2000@users.noreply.github.com>
2020-10-18 21:27:11 +0530

    Understanding while

---
+++ userInput.py
@@ -0,0 +1,7 @@
+name = input('What is your Name ? ')
+liked_color = input('What is your fav color ? ')
+print(name + ' likes ' + liked_color)
+birth_year = input('Birth Year : ')
+age = 2020 - int(birth_year)
+msg = f'{name} likes {liked_color}, he was born on {birth_year}'
+print(msg)
