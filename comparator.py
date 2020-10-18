bd4ac9214e45169e62c3481f334f90c91cff138d
Srivatsa N <11293760+srivatsa2000@users.noreply.github.com>
2020-10-18 20:05:13 +0530

    comparator

--- comparator.py
+++ comparator.py
@@ -6,3 +6,14 @@ elif temperature < 20:
     print("It's a cold day ! ")
 else:
     print("It's a good day")
+
+# Learning
+
+name = input(' What is your name ? ')
+
+if len(name) < 3:
+    print('Name must be at least 3 char long ')
+elif len(name) > 50:
+    print('Name is way too long !')
+else:
+    print('All looks good ! ')
