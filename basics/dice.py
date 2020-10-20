95ad9f70ea1a289e255de38dc9b8e1dbc8a5df8e
Srivatsa N <11293760+srivatsa2000@users.noreply.github.com>
2020-10-20 20:23:38 +0530

    Random

---
+++ basics/dice.py
@@ -0,0 +1,13 @@
+import random
+
+
+class Dice:
+    def roll_dice(self):
+        first = random.randint(1, 6)
+        second = random.randint(1, 6)
+        return first, second
+
+
+dice1 = Dice()
+print(dice1.roll_dice())
+
