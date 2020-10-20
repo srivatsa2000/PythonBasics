39caa0ec3a5ccfb82508ac6960dee68dc48fd88c
Srivatsa N <11293760+srivatsa2000@users.noreply.github.com>
2020-10-20 18:53:35 +0530

    Dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,15 +1,15 @@
 inputValue = input("Type some Digits > ")
 numbersToTextMapping = {
-    "0":"Zero",
-    "1":"One",
-    "2":"Two",
-    "3":"Three",
-    "4":"Four",
-    "5":"Five",
-    "6":"Six",
-    "7":"Seven",
-    "8":"Eight",
-    "9":"Nine"
+    "0": "Zero",
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine"
 }
 
 valuesToPrint = ""
@@ -17,4 +17,3 @@ valuesToPrint = ""
 for digits in inputValue:
     valuesToPrint += numbersToTextMapping.get(digits, "!") + " "
 print(valuesToPrint)
-
