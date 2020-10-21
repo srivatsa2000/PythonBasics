b6865dad9725ec44c51179944b72ce94a8cc31e0
Srivatsa N <11293760+srivatsa2000@users.noreply.github.com>
2020-10-21 10:35:03 +0530

    Random

---
+++ onlineproblems/jadookobadna.py
@@ -0,0 +1,14 @@
+char = input()
+output = ""
+
+for each_letter in char:
+    if each_letter == 'G':
+        output += 'C'
+    elif each_letter == 'C':
+        output += 'G'
+    elif each_letter == 'T':
+        output += 'A'
+    else:
+        output += 'U'
+print(output)
+
